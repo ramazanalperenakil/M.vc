76b901db74a3812589e9bdee1ffe537a2e344591
ramazanalperenakil <ramazanalperenakil@gmail.com>
2021-06-04 13:14:57 +0300

    04.06.2021 60.ders bitti
    61.dersten devam

--- MvcProjeKampi/Controllers/AboutController.cs
+++ MvcProjeKampi/Controllers/AboutController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,29 @@ namespace MvcProjeKampi.Controllers
     public class AboutController : Controller
     {
         // GET: About
+        AboutManger abm = new AboutManger(new EfAboutDal());
         public ActionResult Index()
         {
+            var aboutvalues = abm.GetList();
+            return View(aboutvalues);
+        }
+
+        [HttpGet]
+        public ActionResult AddAbout()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddAbout(About p)
+        {
+            abm.AboutAdd(p);
+            return RedirectToAction("Index");
+        }
+
+        public PartialViewResult AboutPatial()
+        {
+            return PartialView();
         }
     }
 }
